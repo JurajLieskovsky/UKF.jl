842e4059ec55b1c793dc672d0ba6fa66c09b5651
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2024-12-01 14:22:53 +0100

    unscented tranformation

--- src/UKF.jl
+++ src/UKF.jl
@@ -1,5 +1,26 @@
 module UKF
 
-greet() = print("Hello World!")
+using LinearAlgebra
+
+function unscented_transform(x, P)
+    # constants
+    n = length(x)
+    λ = 3 - n
+
+    # weights
+    mean_weights = 1 / (2 * (n + λ)) * ones(2 * n + 1)
+    mean_weights[1] = λ / (n + λ)
+
+    cov_weights = diagm(mean_weights)
+
+    # sigma points
+    S = cholesky(P).L
+    positive = map(s -> x + sqrt(n + λ) * s, eachcol(S))
+    negative = map(s -> x - sqrt(n + λ) * s, eachcol(S))
+
+    return vcat([x], positive, negative), mean_weights, cov_weights
+end
+
+odd_even_direction(i::Int) = (i % 2 == 0) ? 1 : -1
 
 end # module UKF
