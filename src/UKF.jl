6c2d014f66395d3b55a9b7e9b2a1cbdbea2e0b2c
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2024-12-01 22:06:54 +0100

    SigmaPoint struct used everywhere

--- src/UKF.jl
+++ src/UKF.jl
@@ -31,77 +31,41 @@ function unscented_transform(μ, Σ, λ=2, α=1, β=0)
     return vcat([central], positive, negative)
 end
 
-function inverse_unscented_transform(sigma_points)
-    μ = mapreduce(pt -> pt.val * pt.w_μ, +, sigma_points)
-    Σ = mapreduce(pt -> (pt.val - μ) * pt.w_Σ * (pt.val - μ)', +, sigma_points)
-    return μ, Σ
-end
-
-
 function predict(f, μ, P, u, Q)
     # seed sigma points
     sigma_points = unscented_transform(μ, P)
 
     # propagate sigma points through dynamics
-    map(x -> x.val .= f(x.val, u), sigma_points) 
+    map(x -> x.val .= f(x.val, u), sigma_points)
 
     # calculate new mean and covariance
-    new_μ, new_P = inverse_unscented_transform(sigma_points)
+    μn = mapreduce(x -> x.val * x.w_μ, +, sigma_points)
+    Σn = mapreduce(x -> (x.val - μn) * x.w_Σ * (x.val - μn)', +, sigma_points)
 
-    return new_μ, 0.5 * new_P * new_P' + Q
+    return μn, 0.5 * Σn * Σn' + Q
 end
 
 function update(h, μ, P, z, R)
-    # constants
-    n = length(μ)
-    λ = 2
-    α = 1
-    β = 0
+    # seed sigma points
+    sigma_states = unscented_transform(μ, P)
 
-    # auxilary sigma points
-    S = cholesky(P).L
-    positive_states = map(s -> μ + sqrt(n + λ) * s, eachcol(S))
-    negative_states = map(s -> μ - sqrt(n + λ) * s, eachcol(S))
-    auxilary_states = vcat(positive_states, negative_states)
+    # propagate sigma points through measurements
+    sigma_meas = map(x -> SigmaPoint(h(x.val), x.w_μ, x.w_Σ), sigma_states)
 
-    # measurements
-    central_meas = h(μ)
-    auxilary_meas = map(x -> h(x), auxilary_states)
+    # calculate new mean and covariances
+    μ_meas = mapreduce(y -> y.val * y.w_μ, +, sigma_meas)
 
-    # mean measurement
-    w_0 = λ / (n + λ)
-    w_i = 1 / (2 * (n + λ))
-
-    μ_meas = w_0 * central_meas
-    μ_meas += mapreduce(x -> w_i * x, +, auxilary_meas)
-
-    # (in-place) deviations from the mean
-    ## measurements
-    central_meas .= central_meas - μ_meas
-    map(x -> x .= x - μ_meas, auxilary_meas)
-
-    ## states
-    central_state = zeros(eltype(μ), size(μ))
-    map(x -> x .= x - μ, auxilary_states)
-
-    # covariances
-    ## measurement - measurement
-    cov_yy = central_meas * w_0 * central_meas'
-    cov_yy .+= mapreduce(dy -> w_i * dy * dy', +, auxilary_meas)
-    cov_yy .+= R
-
-    # state - measurement
-    cov_xy = central_state * w_0 * central_meas'
-    cov_xy .+= mapreduce((dx, dy) -> w_i * dx * dy', +, auxilary_states, auxilary_meas)
+    Σ_yy = mapreduce(y -> (y.val - μ_meas) * y.w_Σ * (y.val - μ_meas)', +, sigma_meas) + R
+    Σ_xy = mapreduce((x, y) -> (x.val - μ) * y.w_Σ * (y.val - μ_meas)', +, sigma_states, sigma_meas)
 
     # Kalman gain
-    K = cov_xy * inv(cov_yy)
+    K = Σ_xy * inv(Σ_yy)
 
     # Updated state estimate
-    updt_μ = μ - K * (μ_meas - z)
-    updt_P = P - K * cov_xy'
+    μn = μ - K * (μ_meas - z)
+    Σn = P - K * Σ_xy'
 
-    return updt_μ, 0.5 * updt_P * updt_P'
+    return μn, 0.5 * Σn * Σn'
 end
 
 end # module UKF
