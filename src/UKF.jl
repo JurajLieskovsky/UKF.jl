0544859758166135c1f77966035d921e5f2d511a
Juraj Lieskovský <lieskovsky.juraj@gmail.com>
2024-12-02 01:53:19 +0100

    refactored weights

--- src/UKF.jl
+++ src/UKF.jl
@@ -12,12 +12,19 @@ struct SigmaPoint
 end
 
 function weights(n, λ, α, β)
+    # mean
     w_μi = 1 / (2 * (n + λ))
     w_μ0 = λ / (n + λ)
+
+    # covariance
     w_Σi = w_μi
     w_Σ0 = w_μ0 + 1 - α^2 + β
 
-    return w_μi, w_μ0, w_Σi, w_Σ0
+    # concatenated
+    w_μ = vcat(w_μ0, w_μi * ones(2 * n))
+    w_Σ = vcat(w_Σ0, w_Σi * ones(2 * n))
+
+    return w_μ, w_Σ
 end
 
 # Additive unscented transformation, predicion, and update
--- src/additive.jl
+++ src/additive.jl
@@ -4,33 +4,33 @@ Unscented transformation for processes and measurements with additive noise.
 function unscented_transform(μ, Σ, λ=2, α=1, β=0)
     n = length(μ)
 
-    # weights
-    w_μi, w_μ0, w_Σi, w_Σ0 = weights(n, λ, α, β)
-
     # central sigma point
-    central = SigmaPoint(copy(μ), w_μ0, w_Σ0)
+    central = copy(μ)
 
     # auxilary sigma points
     S = cholesky(Σ).L
-    positive = map(s -> SigmaPoint(μ + sqrt(n + λ) * s, w_μi, w_Σi), eachcol(S))
-    negative = map(s -> SigmaPoint(μ - sqrt(n + λ) * s, w_μi, w_Σi), eachcol(S))
+    positive = map(s -> μ + sqrt(n + λ) * s, eachcol(S))
+    negative = map(s -> μ - sqrt(n + λ) * s, eachcol(S))
 
-    return vcat([central], positive, negative)
+    return (
+        vcat([central], positive, negative),
+        weights(n, λ, α, β)...
+    )
 end
 
 """
 Prediction step for processes with additive noise.
 """
-function predict(f, μx, Σx, Σw, u)
-    # sigma points
-    sigma_points = unscented_transform(μx, Σx)
+function predict(f, μx, Σx, μw, Σw, u)
+    # sigma points and their weights
+    seeds, w_μ, w_Σ = unscented_transform(μx, Σx)
 
     # propagation of sigma points through system dynamics
-    map(x -> x.val .= f(x.val, u), sigma_points)
+    sigma_points = map(pt -> f(pt, u) + μw, seeds)
 
     # calculate new mean and covariance
-    μ_new = mapreduce(x -> x.val * x.w_μ, +, sigma_points)
-    Σ_new = mapreduce(x -> (x.val - μ_new) * x.w_Σ * (x.val - μ_new)', +, sigma_points) + Σw
+    μ_new = mapreduce((w, pt) -> pt * w, +, w_μ, sigma_points)
+    Σ_new = mapreduce((w, pt) -> (pt - μ_new) * w * (pt - μ_new)', +, w_Σ, sigma_points) + Σw
 
     return μ_new, 0.5 * Σ_new * Σ_new'
 end
@@ -38,25 +38,25 @@ end
 """
 Update for measurements with additive noise.
 """
-function update(h, μ, P, z, R)
-    # state sigma points
-    sigma_x = unscented_transform(μ, P)
+function update(h, μx, Σx, μv, Σv, z)
+    # state sigma points and sigma point weights
+    sigma_x, w_μ, w_Σ = unscented_transform(μx, Σx)
 
     # measurements from sigma points
-    sigma_y = map(x -> SigmaPoint(h(x.val), x.w_μ, x.w_Σ), sigma_x)
+    sigma_y = map(x -> h(x) + μv, sigma_x)
 
     # new mean and covariances
-    μ_y = mapreduce(y -> y.val * y.w_μ, +, sigma_y)
+    μ_y = mapreduce((w, y) -> y * w, +, w_μ, sigma_y)
 
-    Σ_yy = mapreduce(y -> (y.val - μ_y) * y.w_Σ * (y.val - μ_y)', +, sigma_y) + R
-    Σ_xy = mapreduce((x, y) -> (x.val - μ) * y.w_Σ * (y.val - μ_y)', +, sigma_x, sigma_y)
+    Σ_yy = mapreduce((w, y) -> (y - μ_y) * w * (y - μ_y)', +, w_Σ, sigma_y) + Σv
+    Σ_xy = mapreduce((w, x, y) -> (x - μx) * w * (y - μ_y)', +, w_Σ, sigma_x, sigma_y)
 
     # Kalman gain
     K = Σ_xy * inv(Σ_yy)
 
     # Updated state estimate
-    μ_new = μ - K * (μ_y - z)
-    Σ_new = P - K * Σ_xy'
+    μ_new = μx - K * (μ_y - z)
+    Σ_new = Σx - K * Σ_xy'
 
     return μ_new, 0.5 * Σ_new * Σ_new'
 end
